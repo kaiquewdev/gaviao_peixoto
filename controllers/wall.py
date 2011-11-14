360667cfe37a49ce5be10e4aee62ef131163d6a8
Kaique Silva <kaique.developer@gmail.com>
2011-11-14 21:45:31 -0200

    Project
    - Db, menu and controllers: ok

---
+++ controllers/wall.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def index():
+    return {}
+
+def insert():
+	pass
+
+def edit():
+	pass
---
+++ models/menu.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+#########################################################################
+## Customize your APP title, subtitle and menus here
+#########################################################################
+
+response.title = 'Gavião Peixoto'
+response.subtitle = T('')
+
+## read more at http://dev.w3.org/html5/markup/meta.name.html
+response.meta.author = 'Your Name <you@example.com>'
+response.meta.description = 'a cool new app'
+response.meta.keywords = 'web2py, python, framework'
+response.meta.generator = 'Web2py Web Framework'
+response.meta.copyright = 'Copyright 2011'
+
+## your http://google.com/analytics id
+response.google_analytics_id = None
+
+#########################################################################
+## this is the main application menu add/remove items as required
+#########################################################################
+
+response.menu = [
+    ('Inicio', False, URL('default','index'), []),
+    ('Mural', False, URL('wall','index'), []),
+    ('Blog', False, URL('blog','index'), []),
+    ('Notas', False, URL('notes','index'), []),
+    ('Contato', False, URL('default','contact'), []),
+    ]
